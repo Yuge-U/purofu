b5f0326aa4cbf8f67b12cd157037ee048e3556a8
yukiy <yuki.yuge.0@gmail.com>
2025-03-25 02:47:48 +0900

    PostgreSQL対応: DATABASE_URLの環境変数対応

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app = Flask(__name__)
 
 app.secret_key = "your_secret_key_here"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///messages.db")
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
